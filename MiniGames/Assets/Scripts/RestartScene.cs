db386356421170311ec42f9549dcf677a2157450
irma.maslesa <irma.maslesa@users.noreply.github.com>
2020-04-20 15:06:59 +0200

    Place mi se

--- MiniGames/Assets/Scripts/RestartScene.cs
+++ MiniGames/Assets/Scripts/RestartScene.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,7 +8,6 @@ public class RestartScene : MonoBehaviour
     [SerializeField]
     KeyCode restart = KeyCode.R;
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(restart))
@@ -30,42 +28,3 @@ public class RestartScene : MonoBehaviour
         }
     }
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class RestartScene : MonoBehaviour
-{
-    [SerializeField]
-<<<<<<<< HEAD:MiniGames/Assets/Scripts/RestartScene.cs
-    KeyCode restart = KeyCode.R;
-
-========
-    KeyCode restart;
-    
->>>>>>>> master:MiniGames/Assets/Scripts/SceneMenager.cs
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(restart))
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-            PlayerControler.SetDefaultControls();
-            CameraRotation.TurnOnDefaultCamera();
-        }
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.collider.tag == "Player")
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-            PlayerControler.SetDefaultControls();
-            CameraRotation.TurnOnDefaultCamera();
-        }
-    }
-   
-}
->>>>>>> master
