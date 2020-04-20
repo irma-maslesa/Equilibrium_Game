b5ee7178f3cebf4a307bd0108e9d887d8604c722
irma.maslesa <irma.maslesa@users.noreply.github.com>
2020-04-20 14:27:00 +0200

    Added coins

--- MiniGames/Assets/Scripts/PlayerBehavior.cs
+++ MiniGames/Assets/Scripts/PlayerBehavior.cs
@@ -2,9 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerBehavior : MonoBehaviour
 {
+    public Text collected;
+
+    int coinsCounter;
+    int count;
+
+    private void Start()
+    {
+        count = 0;
+        coinsCounter = GameObject.FindGameObjectsWithTag("Coin").Length;
+        displayCollected();
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.tag == "NPC")
@@ -13,5 +25,29 @@ public class PlayerBehavior : MonoBehaviour
             PlayerControler.SetDefaultControls();
             CameraRotation.TurnOnDefaultCamera();
         }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag == "Coin")
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            displayCollected();
+        }
+            
+    }
+
+    private void displayCollected()
+    {
+        collected.text = $"Collected: {count}/{coinsCounter}";
+
+        if (count == coinsCounter) collected.text = "Collected all, GO TO EXIT!";
+    }
+
+    public bool isAllCollected()
+    {
+        return count == coinsCounter;
     }
 }
