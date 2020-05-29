67dc5d7d9d6ba013880fcf80e8f74f62610f911d
irma.maslesa <irma.maslesa@users.noreply.github.com>
2020-05-30 01:04:16 +0200

    Level lock and save progress implemented

--- MiniGames/Assets/Scripts/GameOver.cs
+++ MiniGames/Assets/Scripts/GameOver.cs
@@ -7,12 +7,17 @@ public class GameOver : MonoBehaviour
 {
     public void PlayAgain()
     {
-        int indexOfStartMenu = 0;
-        SceneManager.LoadScene(indexOfStartMenu);
-
+        PlayerPrefs.SetInt("LevelReached", 1);
+        SceneManager.LoadScene(2);
     }
+
     public void Quit()
     {
         Application.Quit();
+    }
+
+    public void SelectLevel()
+    {
+        SceneManager.LoadScene(1);
     }
 }
--- MiniGames/Assets/Scripts/LevelSelect.cs
+++ MiniGames/Assets/Scripts/LevelSelect.cs
@@ -2,11 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelSelect : MonoBehaviour
 {
+    [SerializeField]
+    Button[] buttons;
+
+    private void Update()
+    {
+        int reachedLevel = PlayerPrefs.GetInt("LevelReached", 1);
+
+        for (int i = 0; i < buttons.Length; i++)
+            if (i + 1 > reachedLevel)
+                buttons[i].interactable = false;
+            else
+                buttons[i].interactable = true;
+    }
+
     public void Load(int SceneIndex)
     {
         SceneManager.LoadScene(SceneIndex);
     }
+
+    public void RestartProgress()
+    {
+        PlayerPrefs.SetInt("LevelReached", 1);
+    }
+
+
 }
